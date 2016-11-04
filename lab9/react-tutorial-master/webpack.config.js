cb25ff6a9c2b2a96fc6012766bdd9f6097fef9dd
Mark Davis <mjd85@hoare.cs.calvin.edu>
2016-11-04 12:56:15 -0400

    completed lab9

--- lab9/react-tutorial-master/webpack.config.js
+++ lab9/react-tutorial-master/webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -15,6 +16,15 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"})
-    ]
+        new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"}),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        port: 3001,
+        proxy: { '/api/*': 'http://localhost:3000' },
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true
+    }
 };
